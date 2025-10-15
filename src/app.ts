95fda7d153a56fadaa395b9152ad095b0d3a0793
bartek <log.mazik.bartek@gmail.com>
2025-10-15 08:43:24 +0200

    init

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("chuj");
+import express from "express";
+import itemRoutes from "./routes/itemRoutes";
+import { errorHandler } from "./middleware/errorHandler";
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.use("/api/items", itemRoutes);
+
+// err handler
+app.use(errorHandler);
+
+export default app;
